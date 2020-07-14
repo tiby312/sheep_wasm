1a94071d36ce255bc0bf21d05b5bab0acb2456aa
Ken <kenakioreed@gmail.com>
2020-07-13 20:16:11 -0400

    websocket

---
+++ src/graphics.rs
@@ -0,0 +1,217 @@
+use web_sys::WebGlBuffer;
+use wasm_bindgen::JsCast;
+use web_sys::{WebGlProgram, WebGl2RenderingContext, WebGlShader};
+use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+
+
+pub struct MyProgram{
+    square_program:WebGlProgram,
+    circle_program:WebGlProgram,
+    buffer:WebGlBuffer
+}
+impl MyProgram{
+    pub fn new(context:&WebGl2RenderingContext)->Result<MyProgram,String>{
+        let vert_shader = compile_shader(
+            &context,
+            WebGl2RenderingContext::VERTEX_SHADER,
+            r#"#version 300 es
+            in vec2 position;
+            out vec2 pos;
+            uniform mat3 mmatrix;
+
+            void main() {
+                gl_PointSize = 40.3;
+                vec3 pp=vec3(position,1.0);
+                gl_Position = vec4(mmatrix*pp, 1.0);
+            }
+        "#,
+        )?;
+        let frag_shader_circle = compile_shader(
+            &context,
+            WebGl2RenderingContext::FRAGMENT_SHADER,
+            r#"#version 300 es
+            precision mediump float;
+            out vec4 out_color;
+            uniform vec4 bg;
+            
+            void main() {
+                //coord is between -0.5 and 0.5
+                vec2 coord = gl_PointCoord - vec2(0.5,0.5);
+                float dissqr=dot(coord,coord);
+                if(dissqr > 0.25){                  //outside of circle radius?
+                    discard;
+                }
+                out_color = bg;
+            }
+        "#,
+        )?;
+
+        let frag_shader_square = compile_shader(
+            &context,
+            WebGl2RenderingContext::FRAGMENT_SHADER,
+            r#"#version 300 es
+            precision mediump float;
+            out vec4 out_color;
+            uniform vec4 bg;
+            
+
+            void main() {
+                //coord is between -0.5 and 0.5
+                vec2 coord = gl_PointCoord - vec2(0.5,0.5);
+
+                float foo=coord.x*coord.x*coord.x*coord.x+coord.y*coord.y*coord.y*coord.y;
+                if(foo > 0.25*0.25){                  //outside of circle radius?
+                    discard;
+                }            
+                out_color = bg;
+            }
+        "#,
+        )?;
+        let buffer = context.create_buffer().ok_or("failed to create buffer")?;
+
+        let circle_program = link_program(&context, &vert_shader, &frag_shader_circle)?;
+        let square_program = link_program(&context, &vert_shader, &frag_shader_square)?;
+        Ok(MyProgram{square_program,circle_program,buffer})
+    }
+    pub fn draw(&mut self,context:&WebGl2RenderingContext,vertices:&[f32],game_dim:[f32;2],as_square:bool){
+
+        
+        let buffer=&self.buffer;
+
+        let program=if as_square{
+            &self.square_program
+        }else{
+            &self.circle_program
+        };
+        context.use_program(Some(program));
+        
+
+        let scalex = 2.0 / game_dim[0];
+        let scaley = 2.0 / game_dim[1];
+        let tx = -1.0;
+        let ty = 1.0;
+        let matrix = [scalex, 0.0, 0.0, 0.0, -scaley, 0.0, tx, ty, 1.0];
+        let mat=context.get_uniform_location(program,"mmatrix");
+        context.uniform_matrix3fv_with_f32_array(mat.as_ref(),false,&matrix);
+        
+
+
+
+        let bf=context.get_uniform_location(program,"bg");
+        context.uniform4fv_with_f32_array(bf.as_ref(),&[1.0,0.0,1.0,1.0]);
+        let foo=  context.get_attrib_location(program, "position") as u32;
+
+        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(buffer));
+        context.vertex_attrib_pointer_with_i32(foo, 2, WebGl2RenderingContext::FLOAT, false, 0, 0);
+        context.enable_vertex_attrib_array(0);
+
+        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(buffer));
+
+        // Note that `Float32Array::view` is somewhat dangerous (hence the
+        // `unsafe`!). This is creating a raw view into our module's
+        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+        // causing the `Float32Array` to be invalid.
+        //
+        // As a result, after `Float32Array::view` we have to be very careful not to
+        // do any memory allocations before it's dropped.
+        unsafe {
+            let vert_array = js_sys::Float32Array::view(  vertices);
+
+            context.buffer_data_with_array_buffer_view(
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &vert_array,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+        }
+
+
+        context.draw_arrays(
+            WebGl2RenderingContext::POINTS,
+            0,
+            (vertices.len()/2) as i32,
+        );
+
+    }
+}
+
+
+fn make_triangle_program(context:&WebGl2RenderingContext)->Result<WebGlProgram,String>{
+    let vert_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        r#"
+        attribute vec4 position;
+        void main() {
+            gl_Position = position;
+        }
+    "#,
+    )?;
+    let frag_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::FRAGMENT_SHADER,
+        r#"
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+        }
+    "#,
+    )?;
+    let triangle_program = link_program(&context, &vert_shader, &frag_shader)?;
+    Ok(triangle_program)
+}
+
+
+
+pub fn compile_shader(
+    context: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    context: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
+
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,204 +2,102 @@ extern crate console_error_panic_hook;
 
 use web_sys::WebGlBuffer;
 
-struct SimpleBuffer{
-    buffer:WebGlBuffer
-}
-impl SimpleBuffer{
-    fn new(context:&WebGl2RenderingContext)->Result<SimpleBuffer,String>{
-        let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-
-        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-        context.vertex_attrib_pointer_with_i32(0, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);
-        context.enable_vertex_attrib_array(0);
 
 
-        Ok(SimpleBuffer{buffer})
-    }
+mod graphics;
+mod console;
+use graphics::*;
 
-    fn draw(&self,vertices:&[f32],context:&WebGl2RenderingContext,program:&WebGlProgram){
-        context.use_program(Some(&program));
-
-        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&self.buffer));
-
-
-        // Note that `Float32Array::view` is somewhat dangerous (hence the
-        // `unsafe`!). This is creating a raw view into our module's
-        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-        // causing the `Float32Array` to be invalid.
-        //
-        // As a result, after `Float32Array::view` we have to be very careful not to
-        // do any memory allocations before it's dropped.
-        unsafe {
-            let vert_array = js_sys::Float32Array::view(&vertices);
-
-            context.buffer_data_with_array_buffer_view(
-                WebGl2RenderingContext::ARRAY_BUFFER,
-                &vert_array,
-                WebGl2RenderingContext::STATIC_DRAW,
-            );
-        }
-
-
-        context.draw_arrays(
-            WebGl2RenderingContext::TRIANGLES,
-            0,
-            (vertices.len() / 3) as i32,
-        );
-
-    }
-}
-
-
-pub struct MyProgram{
-    program:WebGlProgram
-}
-impl MyProgram{
-    pub fn new(context:&WebGl2RenderingContext)->Result<MyProgram,String>{
-        let vert_shader = compile_shader(
-            &context,
-            WebGl2RenderingContext::VERTEX_SHADER,
-            r#"#version 300 es
-            in vec2 position;
-            out vec2 pos;
-            void main() {
-                gl_PointSize = 20.3;
-                vec3 pp=vec3(position,1.0);
-                gl_Position = vec4(pp, 1.0);
-            }
-        "#,
-        )?;
-        let frag_shader = compile_shader(
-            &context,
-            WebGl2RenderingContext::FRAGMENT_SHADER,
-            r#"#version 300 es
-            precision mediump float;
-            out vec4 out_color;
-            uniform vec4 bg;
-            void main() {
-            
-                vec2 coord = gl_PointCoord - vec2(0.5,0.5);
-                float dis=dot(coord,coord);
-                if(dis > 0.25){                  //outside of circle radius?
-                    discard;
-                }
-            
-                out_color = bg;
-            }
-        "#,
-        )?;
-        let program = link_program(&context, &vert_shader, &frag_shader)?;
-        Ok(MyProgram{program})
-    }
-    pub fn draw(&self,context:&WebGl2RenderingContext,buffer:&WebGlBuffer,vertices:&[f32]){
-        context.use_program(Some(&self.program));
-        
-        let bf=context.get_uniform_location(&self.program,"bg");
-        context.uniform4fv_with_f32_array(bf.as_ref(),&[1.0,0.0,1.0,1.0]);
-        let foo=  context.get_attrib_location(&self.program, "position") as u32;
-
-        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(buffer));
-        context.vertex_attrib_pointer_with_i32(foo, 2, WebGl2RenderingContext::FLOAT, false, 0, 0);
-        context.enable_vertex_attrib_array(0);
-
-        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(buffer));
-
-        // Note that `Float32Array::view` is somewhat dangerous (hence the
-        // `unsafe`!). This is creating a raw view into our module's
-        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-        // causing the `Float32Array` to be invalid.
-        //
-        // As a result, after `Float32Array::view` we have to be very careful not to
-        // do any memory allocations before it's dropped.
-        unsafe {
-            let vert_array = js_sys::Float32Array::view(  vertices);
-
-            context.buffer_data_with_array_buffer_view(
-                WebGl2RenderingContext::ARRAY_BUFFER,
-                &vert_array,
-                WebGl2RenderingContext::STATIC_DRAW,
-            );
-        }
-
-
-        context.draw_arrays(
-            WebGl2RenderingContext::POINTS,
-            0,
-            (vertices.len()/2) as i32,
-        );
-
-    }
-}
-
-
-fn make_triangle_program(context:&WebGl2RenderingContext)->Result<WebGlProgram,String>{
-    let vert_shader = compile_shader(
-        &context,
-        WebGl2RenderingContext::VERTEX_SHADER,
-        r#"
-        attribute vec4 position;
-        void main() {
-            gl_Position = position;
-        }
-    "#,
-    )?;
-    let frag_shader = compile_shader(
-        &context,
-        WebGl2RenderingContext::FRAGMENT_SHADER,
-        r#"
-        void main() {
-            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-        }
-    "#,
-    )?;
-    let triangle_program = link_program(&context, &vert_shader, &frag_shader)?;
-    Ok(triangle_program)
-}
-
-
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-
-// Next let's define a macro that's like `println!`, only it works for
-// `console.log`. Note that `println!` doesn't actually work on the wasm target
-// because the standard library currently just eats all output. To get
-// `println!`-like behavior in your app you'll likely want a macro like this.
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
 
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{WebGlProgram, WebGl2RenderingContext, WebGlShader};
 use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+use shclient_gen::*;
+
+static mut STATE: Option<Manager> = None;
+
+
+#[wasm_bindgen]
+pub fn game_initial(gameid:u32,name:js_sys::JsString)->js_sys::ArrayBuffer{
+    let gameid=GameID(gameid);
+    
+    let name=PlayerName(name.into());
+    let k=ClientToServer::JoinRequest{gameid,name};
+    let bytes=bincode::serialize(&k).unwrap();
+    let k:js_sys::Array=bytes.into_iter().map(|a|JsValue::from(a)).collect();
+    js_sys::Uint8Array::new(&k).buffer()
+}
+
+use crate::console::log;
+#[wasm_bindgen]
+pub fn game_new(gameid:u32,name:js_sys::JsString,a:js_sys::ArrayBuffer,b:js_sys::ArrayBuffer){
+    console_log!("YOOO");
+    let gameid=GameID(gameid);
+    let a=unsafe{
+        js_sys::Uint8Array::new(&a).to_vec()
+    };
+    let b=unsafe{
+        js_sys::Uint8Array::new(&b).to_vec()
+    };
+    console_log!("YOOO");
+    let a:ServerToClient=bincode::deserialize(&a).map(|a|{println!("Received {:?}",a);a}).unwrap();
+    let b:ServerToClient=bincode::deserialize(&b).map(|b|{println!("Received {:?}",b);b}).unwrap();
+    console_log!("YOOO");
+    let name=PlayerName(name.into());
+    unsafe{
+        STATE=Some(Manager::new(gameid,name,a,b));
+    }
+    console_log!("YOOO");
+}
+
+#[wasm_bindgen]
+pub fn game_premove(x:f32,y:f32,clicked:bool)->Option<js_sys::ArrayBuffer>{
+    let mycommit=if clicked{
+        Some([x,y])
+    }else{
+        None
+    };
+
+    let k=unsafe{STATE.as_mut().unwrap().premove(mycommit)};
+    if let Some(k)=k{
+        let bytes=bincode::serialize(&k).unwrap();
+        let k:js_sys::Array=bytes.into_iter().map(|a|JsValue::from(a)).collect();
+        Some(js_sys::Uint8Array::new(&k).buffer())
+    }else{
+        None
+    }
+    
+}
+
+
+#[wasm_bindgen]
+pub fn game_process(s:Option<js_sys::ArrayBuffer>){
+    let a=if let Some(a)=s{
+        let a=unsafe{
+            js_sys::Uint8Array::new(&a).to_vec()
+        };
+        let a:ServerToClient=bincode::deserialize(&a).map(|a|{println!("Received {:?}",a);a}).unwrap();
+        Some(a)
+    }else{
+        None
+    };
+    unsafe{STATE.as_mut().unwrap().recv(a)}
+}
+
+#[wasm_bindgen]
+pub fn game_draw(){
+    let game=unsafe{STATE.as_ref().unwrap().get_game()};
+    //TODO draw
+}
+
+
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    log("YOO WATUPS");
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
@@ -212,17 +110,21 @@ pub fn main() -> Result<(), JsValue> {
         .dyn_into::<WebGl2RenderingContext>()?;
 
     
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-
-    let p=MyProgram::new(&context)?;
-
-    let vertices= [-0.7f32, -0.7, 0.7, -0.7, 0.0, 0.7];
     
+    let mut p=MyProgram::new(&context)?;
+
+    //let vertices= [-0.7f32, -0.7, 0.7, -0.7, 0.0, 0.7];
+
+    let vertices= [400.0, 500.0];
+
     context.clear_color(0.0, 0.0, 0.0, 1.0);
     context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
-    //TODO assert point size max is big enough.
-    p.draw(&context,&buffer,&vertices);
+    let dim=[800.0,600.0];
+    p.draw(&context,&vertices,dim,true);
+    
+
+    
     /*
     let triangle_program=make_triangle_program(&context)?;
     
@@ -238,57 +140,3 @@ pub fn main() -> Result<(), JsValue> {
     
     Ok(())
 }
-
-pub fn compile_shader(
-    context: &WebGl2RenderingContext,
-    shader_type: u32,
-    source: &str,
-) -> Result<WebGlShader, String> {
-    let shader = context
-        .create_shader(shader_type)
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
-
-    if context
-        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(shader)
-    } else {
-        Err(context
-            .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
-    }
-}
-
-pub fn link_program(
-    context: &WebGl2RenderingContext,
-    vert_shader: &WebGlShader,
-    frag_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
-    let program = context
-        .create_program()
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
-
-    if context
-        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(program)
-    } else {
-        Err(context
-            .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
-    }
-}
-
-
-
-
